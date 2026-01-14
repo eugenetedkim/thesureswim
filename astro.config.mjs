b2b1158e96d85137dcfbb6b20f0a8ed3317b4828
eugenetedkim <eugenetedkim@gmail.com>
2026-01-13 17:09:51 -0800

    Deploy Astro static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
-import github from "@astrojs/github";
 
 export default defineConfig({
   output: "static",
-  adapter: github(),
   site: "https://thesureswim.com",
-  base: "/", // REQUIRED for custom domain
+  base: "/",
 });
